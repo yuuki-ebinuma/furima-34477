69ee8057d4a8a78ee621c9cfcef615954778db73
yuuki-ebinuma <78467979+yuuki-ebinuma@users.noreply.github.com>
2021-03-08 19:12:29 +0900

    商品編集機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!,only: [:new,:delete,:create]
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_product, only:[:show,:edit,:update]
+  before_action :move_to_index, only:[:edit,:update]
 
   def index
     @products = Product.all.order(updated_at: :desc)
@@ -19,7 +21,17 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
   end
 
 
@@ -28,6 +40,16 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:image,:product_name,:product_description,:category_id,:ship_city_id,:ship_date_id,:ship_charge_id,:product_status_id,:amount_sold).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @product.user_id
+      redirect_to root_path
+    end
   end
   
 end
